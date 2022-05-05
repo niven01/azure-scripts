8e363940fed729c189b2366b5c58e19fadc3c575
Nielsen Pierce <nielsenpi@softcat.com>
2022-05-05 13:38:48 +0100

    Update prep-linux-disks.sh

--- prep-linux-disks.sh
+++ prep-linux-disks.sh
@@ -5,7 +5,7 @@ do
     then
         echo "Disk Exists"
     else
-        echo "y" | mkfs.ext4 /dev/disk/azure/scsi1/${LUN}
+        echo "y" | mkfs.ext4 /dev/disk/azure/scsi1/$LUN
         mkdir /data${NUM}
         echo "/dev/disk/azure/scsi1/${LUN} /data${NUM} ext4 defaults,nofail 0 0" >>/etc/fstab
     fi
